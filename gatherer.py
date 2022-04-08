99ce7574e8f1abafb3e4e779b4ee53ebb7c5afc7
Rodrigo Formigon <rodris.hud@gmail.com>
2022-04-08 08:37:39 -0300

    Update gatherer.py
    Now card-gatherer can use the entire table from ligamagic.com without any manual treatment. With this update, the program can find if any card number is attached to a letter and consider just the number, and after all, converts everything to an int type.

--- gatherer.py
+++ gatherer.py
@@ -1,35 +1,42 @@
-from unicodedata import numeric
 import pandas as pd
 import sys
+from math import isnan
+import numpy as np
 
 # Define a função que lê a tabela em .csv
 def readTable(tableName):
-    tabela = pd.read_csv(tableName)
-    tabela['Card #'] = pd.to_numeric(tabela['Card #'], errors='coerce')
-    return tabela['Card #']
-
+    table = pd.read_csv(tableName)
+    table = table['Card #']
+    # Verifica se o card tem o caractere "a" junto ao número e o separa, atribuindo o novo valor à tabela
+    for i, card in enumerate(table):
+        if 'a' in card:
+            temp = table[i].split('a')
+            table[i] = temp[0]
+    # Transforma os valores da coluna 'Card #' em numéricos
+    table = pd.to_numeric(table, errors='coerce')
+    # Remove as linhas com valores nulos
+    for i, card in enumerate(table):
+        if isnan(card):
+            table = table.drop(i)
+    # Converte os valores para inteiros
+    table = table.astype(np.int64)
+    return table
 
 # Define a função que prepara a lista de cards adquiridos
-def listaAdquiridos(tabelaCard):
-
+def listaAdquiridos(table):
     lista_adquiridos = []
-
-    for item in tabelaCard:
+    for item in table:
         if item not in lista_adquiridos:
             lista_adquiridos.append(item)
-            
     lista_adquiridos.sort()
     return lista_adquiridos
 
 # Define a função que prepara a lista de cards faltantes
-def listaFaltantes(lista, total):
-    
+def listaFaltantes(list, total):
     lista_faltantes = []
-    
     for numero in range(total):
-        if numero+1 not in lista:
+        if numero+1 not in list:
             lista_faltantes.append(numero+1)
-
     return lista_faltantes
 
 
@@ -38,10 +45,10 @@ if len(sys.argv) != 3:
     print(f'USAGE: python <table> <total cards>')
     exit()
 
-tabelaCard = readTable(sys.argv[1])
+tabela = readTable(sys.argv[1])
 total = int(sys.argv[2])
 
-adquiridos = listaAdquiridos(tabelaCard)
+adquiridos = listaAdquiridos(tabela)
 faltantes = listaFaltantes(adquiridos, total)
 
 print()
